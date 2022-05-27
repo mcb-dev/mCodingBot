354d6ff47b8e2cd356956c865e418928cadd9da8
Lunarmagpie <Bambolambo0@gmail.com>
2022-05-27 18:51:14 -0400

    fix workflows

--- mcodingbot/plugins/reactions.py
+++ mcodingbot/plugins/reactions.py
@@ -1,6 +1,7 @@
+import re
+
 import crescent
 import hikari
-import re
 
 RUST_REGEX = re.compile(r"\brust\b", flags=re.I)
 
@@ -9,11 +10,14 @@ plugin = crescent.Plugin("reactions")
 
 @plugin.include
 @crescent.event
-async def on_message(event: hikari.MessageCreateEvent):
+async def on_message(event: hikari.MessageCreateEvent) -> None:
     message = event.message
 
     if message.type == hikari.MessageType.GUILD_MEMBER_JOIN:
         await message.add_reaction("👋")
+        return
+
+    if not message.content:
         return
 
     if RUST_REGEX.search(message.content) and "🚀" in message.content:

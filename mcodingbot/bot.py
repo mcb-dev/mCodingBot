884ef01b8e11e99aa90eaae7c6146683e171cc06
Circuit <circuitsacul@icloud.com>
2022-05-29 18:57:18 -0400

    Rename basic plugin to info

--- mcodingbot/bot.py
+++ mcodingbot/bot.py
@@ -7,5 +7,5 @@ class Bot(crescent.Bot):
     def __init__(self) -> None:
         super().__init__(token=CONFIG.discord_token)
 
-        self.plugins.load("mcodingbot.plugins.basic")
+        self.plugins.load("mcodingbot.plugins.info")
         self.plugins.load("mcodingbot.plugins.reactions")

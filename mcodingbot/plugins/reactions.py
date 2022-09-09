2bf6691da039296cceae2b61f64d8986646e1e54
FuexFollets <fuex9follets@gmail.com>
2022-09-09 16:08:21 +0000

    organized reactions in functions

--- mcodingbot/plugins/reactions.py
+++ mcodingbot/plugins/reactions.py
@@ -13,12 +13,24 @@ plugin = Plugin()
 @plugin.include
 @crescent.event
 async def on_message(event: hikari.MessageCreateEvent) -> None:
+    for add_reaction in reaction_checks:
+        if add_reaction(event):
+            break
+    
+def reaction_welcome(event: hikari.MessageCreateEvent) -> bool:
     if event.message.type is hikari.MessageType.GUILD_MEMBER_JOIN:
         await event.message.add_reaction("👋")
+        return True
+    return False
 
-    elif (
+def reaction_rust(event: hikari.MessageCreateEvent) -> bool:
+    if (
         event.message.content
         and RUST_REGEX.search(event.message.content)
         and "🚀" in event.message.content
     ):
         await event.message.add_reaction("🚀")
+        return True
+    return False
+
+reaction_checks: tuple[Callable] = (reaction_welcome, reaction_rust)

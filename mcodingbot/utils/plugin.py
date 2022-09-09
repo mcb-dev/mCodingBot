4a2cbb02d2852635faa0ce3993bf298f3532846d
trag1c <77130613+trag1c@users.noreply.github.com>
2022-09-09 10:34:30 +0200

    Made typing imports consistent

--- mcodingbot/utils/plugin.py
+++ mcodingbot/utils/plugin.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
-import typing
+from typing import cast, Sequence, TYPE_CHECKING
 
 import crescent
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from mcodingbot.bot import Bot
 
-__all__: typing.Sequence[str] = ("Plugin",)
+__all__: Sequence[str] = ("Plugin",)
 
 
 class Plugin(crescent.Plugin):
     @property
     def app(self) -> Bot:
-        return typing.cast("Bot", super().app)
+        return cast("Bot", super().app)

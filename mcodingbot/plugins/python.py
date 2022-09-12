3ec4e01fed2a8de0f3c80bbfcfe98c6ee704e971
Qexat <contact@qexat.com>
2022-09-12 13:43:19 +0200

    feat: auto link to mentioned PEPs in chat

--- mcodingbot/plugins/python.py
+++ mcodingbot/plugins/python.py
@@ -1,8 +1,21 @@
+import re
+
 import crescent
+import hikari
 
 from mcodingbot.utils import Plugin
 
 plugin = Plugin()
+
+pep_regex = re.compile(r"pep *\d{1,4}", re.IGNORECASE)
+
+
+def extract_pep_number(ref: str) -> int:
+    return int(ref.casefold().removeprefix("pep").strip())
+
+
+def get_pep_link(pep_number: int) -> str:
+    return f"<https://peps.python.org/pep-{pep_number:03}/>"
 
 
 @plugin.include
@@ -11,4 +24,28 @@ class PEPCommand:
     pep_number = crescent.option(int, "the PEP number")
 
     async def callback(self, ctx: crescent.Context) -> None:
-        await ctx.respond(f"https://peps.python.org/pep-{self.pep_number:03}/")
+        await ctx.respond(get_pep_link(self.pep_number))
+
+
+@plugin.include
+@crescent.event
+async def on_message(event: hikari.MessageCreateEvent) -> None:
+    if event.message.content is not None:
+        pep_refs: list[int] = [
+            extract_pep_number(ref.group())
+            for ref in re.finditer(pep_regex, event.message.content)
+        ]
+
+        if not pep_refs:
+            return
+
+        pep_links_message: str = "\n".join(
+            [
+                f"PEP {pep_number}: {pep_link}"
+                for pep_number, pep_link in zip(
+                    pep_refs, map(get_pep_link, pep_refs)
+                )
+            ]
+        )
+
+        await event.message.respond(pep_links_message)

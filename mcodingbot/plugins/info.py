fda622f0b61ca24b5965da601885aa4f164630e2
Qexat <contact@qexat.com>
2022-09-12 11:30:32 +0200

    fix: PEP number zero padding

--- mcodingbot/plugins/info.py
+++ mcodingbot/plugins/info.py
@@ -36,4 +36,4 @@ class PEPCommand:
     pep_number = crescent.option(int, "the PEP number")
 
     async def callback(self, ctx: crescent.Context) -> None:
-        await ctx.respond(f"https://peps.python.org/pep-{self.pep_number}/")
+        await ctx.respond(f"https://peps.python.org/pep-{self.pep_number:03}/")

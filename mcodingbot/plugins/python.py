40df1956acdda9898bdc5d23be5cbe8d9b2764f5
Lunarmagpie <bambolambo0@gmail.com>
2022-09-12 19:42:01 -0400

    remove shadowing

--- mcodingbot/plugins/python.py
+++ mcodingbot/plugins/python.py
@@ -80,9 +80,7 @@ async def on_interaction(event: hikari.InteractionCreateEvent) -> None:
     ):
         return
 
-    id = decode_dismiss_button_id(inter.custom_id)
-
-    if inter.user.id != id:
+    if inter.user.id != decode_dismiss_button_id(inter.custom_id):
         await inter.create_initial_response(
             hikari.ResponseType.MESSAGE_CREATE,
             "Only the original message's author can dismiss this message.",

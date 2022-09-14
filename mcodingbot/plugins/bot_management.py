a43102f14eb0c6a514fc1f2770bb4be7350de513
Lunarmagpie <bambolambo0@gmail.com>
2022-09-13 22:04:36 -0400

    among us

--- mcodingbot/plugins/bot_management.py
+++ mcodingbot/plugins/bot_management.py
@@ -16,7 +16,6 @@ plugin = crescent.Plugin()
     dm_enabled=False,
     guild=CONFIG.mcoding_server,
 )
-class RestartBot:
-    async def callback(self, ctx: crescent.Context) -> None:
-        await ctx.respond("Restarting bot...")
-        await ctx.app.close()
+async def restart_bot(ctx: crescent.Context) -> None:
+    await ctx.respond("Restarting bot...")
+    await ctx.app.close()

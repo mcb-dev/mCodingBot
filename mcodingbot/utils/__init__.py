26d5dff23a21c6ecd410337c59b26ed6ff24fbec
Lunarmagpie <bambolambo0@gmail.com>
2022-09-14 10:37:26 -0400

    apply lint again

--- mcodingbot/utils/__init__.py
+++ mcodingbot/utils/__init__.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 
-from mcodingbot.utils.plugins import Context, Plugin
 from mcodingbot.utils.peps import PEPInfo, PEPManager
+from mcodingbot.utils.plugins import Context, Plugin
 
-__all__: Sequence[str] = ("Context", "Plugin", "PEPManager", "PEPInfo")
+__all__: Sequence[str] = ("PEPManager", "PEPInfo", "Context", "Plugin")

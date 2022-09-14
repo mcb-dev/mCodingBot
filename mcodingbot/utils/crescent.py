76a3de0bf12e539d4a7a03a35de4af30f3c64d24
Lunarmagpie <bambolambo0@gmail.com>
2022-09-13 23:13:31 -0400

    skill issue

---
+++ mcodingbot/utils/crescent.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Sequence, cast
+
+import crescent
+
+if TYPE_CHECKING:
+    from mcodingbot.bot import Bot
+
+__all__: Sequence[str] = ("Plugin", "Context")
+
+
+class Plugin(crescent.Plugin):
+    @property
+    def app(self) -> Bot:
+        return cast("Bot", super().app)
+
+class Context(crescent.Context):
+    app: Bot

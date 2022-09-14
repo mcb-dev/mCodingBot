2ecf3a37c256d56a9779ecb3f229024d50a528ee
Lunarmagpie <bambolambo0@gmail.com>
2022-09-14 10:34:47 -0400

    crescent->plugins

--- mcodingbot/utils/__init__.py
+++ mcodingbot/utils/__init__.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 
-from mcodingbot.utils.crescent import Context, Plugin
+from mcodingbot.utils.plugins import Context, Plugin
 from mcodingbot.utils.peps import PEPInfo, PEPManager
 
 __all__: Sequence[str] = ("Context", "Plugin", "PEPManager", "PEPInfo")

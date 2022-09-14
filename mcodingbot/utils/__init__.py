59ff300e0efb6d7dfb9d8dd487b189395a604465
Lunarmagpie <bambolambo0@gmail.com>
2022-09-14 10:37:44 -0400

    match import order

--- mcodingbot/utils/__init__.py
+++ mcodingbot/utils/__init__.py
@@ -3,4 +3,4 @@ from typing import Sequence
 from mcodingbot.utils.peps import PEPInfo, PEPManager
 from mcodingbot.utils.plugins import Context, Plugin
 
-__all__: Sequence[str] = ("PEPManager", "PEPInfo", "Context", "Plugin")
+__all__: Sequence[str] = ("PEPInfo", "PEPManager", "Context", "Plugin")

12c123c19ec91429de9197d19fd68c271e7b227e
Lunarmagpie <Bambolambo0@gmail.com>
2022-09-15 13:16:00 -0400

    Make class commands into functions (#57)
    Co-authored-by: CircuitSacul <circuitsacul@icloud.com>

--- mcodingbot/plugins/info.py
+++ mcodingbot/plugins/info.py
@@ -9,22 +9,20 @@ plugin = Plugin()
 
 @plugin.include
 @crescent.command(name="ping", description="Pong!")
-class PingCommand:
-    async def callback(self, ctx: Context) -> None:
-        await ctx.respond(f"Pong! {round(ctx.app.heartbeat_latency*1000)} ms.")
+async def ping(ctx: Context) -> None:
+    await ctx.respond(f"Pong! {round(ctx.app.heartbeat_latency*1000)} ms.")
 
 
 @plugin.include
 @crescent.command(name="links", description="Useful links")
-class LinksCommand:
-    async def callback(self, ctx: Context) -> None:
-        embed = hikari.Embed(
-            title="Links",
-            color=CONFIG.theme,
-            description=(
-                f"**[mCoding Youtube]({CONFIG.mcoding_youtube})**\n"
-                f"**[mCoding repo]({CONFIG.mcoding_repo})**\n"
-                f"**[mCodingBot repo]({CONFIG.mcodingbot_repo})**"
-            ),
-        )
-        await ctx.respond(embed=embed)
+async def links(ctx: Context) -> None:
+    embed = hikari.Embed(
+        title="Links",
+        color=CONFIG.theme,
+        description=(
+            f"**[mCoding Youtube]({CONFIG.mcoding_youtube})**\n"
+            f"**[mCoding repo]({CONFIG.mcoding_repo})**\n"
+            f"**[mCodingBot repo]({CONFIG.mcodingbot_repo})**"
+        ),
+    )
+    await ctx.respond(embed=embed)

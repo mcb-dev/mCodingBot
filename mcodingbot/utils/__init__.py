403e485a2992ca5a6ce6f57467fa4afd06709d0e
Endercheif <reet22singh@gmail.com>
2022-09-14 22:41:00 -0700

    fixed issue with linting

--- mcodingbot/utils/__init__.py
+++ mcodingbot/utils/__init__.py
@@ -4,4 +4,10 @@ from mcodingbot.utils.peps import PEPInfo, PEPManager
 from mcodingbot.utils.plugins import Context, Plugin
 from mcodingbot.utils.search import fuzzy_search
 
-__all__: Sequence[str] = ("PEPInfo", "PEPManager", "Context", "Plugin", "fuzzy_search")
+__all__: Sequence[str] = (
+    "PEPInfo",
+    "PEPManager",
+    "Context",
+    "Plugin",
+    "fuzzy_search",
+)

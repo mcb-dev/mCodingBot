00d4efbf583ab7b3be2b3ede598bf78b9075c577
Lunarmagpie <Bambolambo0@gmail.com>
2022-09-25 00:45:54 -0400

    gammar

--- mcodingbot/utils/peps.py
+++ mcodingbot/utils/peps.py
@@ -106,7 +106,7 @@ class PEPInfo:
 
         name = self.title
         if len(name) > max_name_length:
-            # an extra 3 chars need to be removed to make space for the
+            # An extra 3 chars need to be removed to make space for the
             # ellipsis.
             name = f"{self.title[:max_name_length - 3]}..."
 

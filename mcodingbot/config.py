3d1c8f88df32ad9833e825a52042cd8866999502
Lunarmagpie <Bambolambo0@gmail.com>
2022-12-24 14:12:38 -0500

    site -> website

--- mcodingbot/config.py
+++ mcodingbot/config.py
@@ -39,7 +39,7 @@ class Config:
     mcoding_yt_id: str = "YOUTUBE_CHANNEL_ID"
     yt_api_key: str = "YOUTUBE_API_KEY"
 
-    mcoding_site: str = "https://mcoding.io"
+    mcoding_website: str = "https://mcoding.io"
     mcoding_youtube: str = "https://www.youtube.com/@mCoding"
     mcoding_repo: str = "https://github.com/mCodingLLC/VideosSampleCode"
     mcodingbot_repo: str = "https://github.com/mcb-dev/mCodingBot"
--- mcodingbot/plugins/info.py
+++ mcodingbot/plugins/info.py
@@ -20,7 +20,7 @@ async def links(ctx: Context) -> None:
         title="Links",
         color=CONFIG.theme,
         description=(
-            f"**[mCoding site]({CONFIG.mcoding_site})**\n"
+            f"**[mCoding website]({CONFIG.mcoding_website})**\n"
             f"**[mCoding Youtube]({CONFIG.mcoding_youtube})**\n"
             f"**[mCoding repo]({CONFIG.mcoding_repo})**\n"
             f"**[mCodingBot repo]({CONFIG.mcodingbot_repo})**"
